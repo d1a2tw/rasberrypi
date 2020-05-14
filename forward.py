f2812f9c4757efb2a7469a084f0fed1b028bf79e
d1a2tw <d1a2tw@gmail.com>
2020-05-14 20:55:29 +0800

    123

--- forward.py
+++ forward.py
@@ -1,11 +1,11 @@
 """Simple test for using adafruit_motorkit with a DC motor"""
 import time
-from adafruit_motorkit import MotorKit
+from adafruit_motorkit import MotorKit                  
  
-kit = MotorKit()
+kit = MotorKit()                                ## wheel1 =right    ,wheel 2 = left
  
-kit.motor1.throttle = 1.0           ## set left wheel speed
-kit.motor2.throttle = 1.0           ## set right wheel speed
-time.sleep(5)
-kit.motor1.throttle = 0             ## left wheel stop
-kit.motor2.throttle = 0             ## right wheel stop
+kit.motor1.throttle = 0.5           ## set wheel1 speed
+kit.motor2.throttle = 0.5         ## set  wheel2  speed
+time.sleep(0.2)
+kit.motor1.throttle = 0             ## wheel 1  stop
+kit.motor2.throttle = 0             ## wheel 2  stop
---
+++ turnright_image/turnright_script.py
@@ -0,0 +1,23 @@
+import os 
+import time
+from adafruit_motorkit import MotorKit
+
+folder_name="5_14_turnleft"     ## image saved name     =>        "  date +action type"
+times=100                                             ## number of image
+kit = MotorKit()                                    ## wheel1 =right    ,wheel 2 = left
+
+
+
+
+for i in range (times):
+    kit.motor1.throttle = 0              ## set wheel1 speed
+    kit.motor2.throttle = 0.5           ## set  wheel 2 speed
+    time.sleep(0.2)
+    kit.motor1.throttle = 0             ##  wheel1  stop
+    kit.motor2.throttle = 0             ##  wheel 2stop
+    a="raspistill -o  "
+    b=folder_name
+    c=str(i)                    # the ith photo
+    d=".jpg"
+    command=a+b+c+d
+    os.system(command)
